bdd6422837c664925178ca1aff648b1cda8626fb
Jordan Roberts <jordanroberts@talk21.com>
2019-10-05 13:26:23 +0100

    Added WriteXML method

--- Serialization/Program.cs
+++ Serialization/Program.cs
@@ -2,15 +2,31 @@
 
 namespace Serialization
 {
-    class XMLWriter
+    public class XMLWriter
     {
         static void Main(string[] args)
         {
+            WriteXML();
         }
-    }
 
-    public class Book
-    {
-        public String title;
+
+        public class Book
+        {
+            public String title;
+        }
+
+        public static void WriteXML()
+        {
+            Book book = new Book();
+            book.title = "Jordan's book";
+            System.Xml.Serialization.XmlSerializer writer =
+                new System.Xml.Serialization.XmlSerializer(typeof(Book));
+
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "//SerializationOverview.xml";
+            System.IO.FileStream file = System.IO.File.Create(path);
+
+            writer.Serialize(file, book);
+            file.Close();
+        }
     }
 }
